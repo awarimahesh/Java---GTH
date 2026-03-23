4e2a07e960c355226aa9006a8f91f4398d24cfda
awarimahesh <163688271+awarimahesh@users.noreply.github.com>
2026-03-23 16:25:37 +0530

    code

---
+++ Practice/TCS/Q23.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Rd{
+public static void main(String args[]){
+	int a[]={1,2,3,4,5,3,3,4};
+	
+	for(int i=0;i<a.length;i++){
+		int(j=i+1;j<a.length;j++){
+			if(a[i]==a[j]){
+				for(int k=j;k<a.lenght-1;k++){
+					if(a[k]==a[k+1]){
+						
+					}
+					a.length--;
+					j--;
+					
+				}
+			}
+		}
+	}
+	for(int i=o;i<a.length;i++){
+		System.out.println(a[i]);
+	}
+}
+}
